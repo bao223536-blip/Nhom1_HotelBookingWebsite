348a6b4ac53626f0632b63e0d8a0d05fb76c365a
Bao_2 <bao223536@gmail.com>
2025-11-22 15:52:20 +0700

    Update logout.php

--- server/api/logout.php
+++ server/api/logout.php
@@ -1,12 +1,22 @@
 <?php
-// logout.php
+// logout.php 
 session_start();
 $_SESSION = [];
 session_unset();
 session_destroy();
 
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) || 
+    (isset($_SERVER['HTTP_ACCEPT']) && strpos($_SERVER['HTTP_ACCEPT'], 'application/json') !== false)) {
+
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode([
+        'status' => 'success',
+        'message' => 'Đã đăng xuất'
+    ]);
+    exit;
+}
+
 $msg = urlencode('Đã đăng xuất');
-$typ = 'success';
-header("Location: ../client/login.html?msg={$msg}&type={$typ}");
+header('Location: ../client/login.html?msg=' . $msg . '&type=success');
 exit;
 ?>
